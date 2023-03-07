f17f772839b00b2e60c9f360095b96494bcd84b3
calculator001 <calvinokumu05@gmail.com>
2023-03-06 22:01:14 -0800

    Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
-gcc -masm=intel $CFILE -S > $(echo $CFILE|cut -d"." -f1).s
+#include <unistd.h>
+
+/**
+ * main - Program entry point
+ * Description: Prints the specified string
+ * Return: 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
