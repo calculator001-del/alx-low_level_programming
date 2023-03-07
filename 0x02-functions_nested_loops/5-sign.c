0c696b2f261e43489c9b550b161fe1fe2c6332c6
calculator001 <calvinokumu05@gmail.com>
2023-03-06 22:29:58 -0800

    Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,30 @@
+#include <stdio.h>
 #include "main.h"
 
+int print_sign(int n);
+
 /**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * print_sign - prints the integer sign
+ * @n: this integer will be tested
+ * Description: +,- & 0 for positive, negative & zero
+ * Return: void
  */
+
 int print_sign(int n)
 {
-	if (n > 0)
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n > 0)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
-}
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
 	}
-		return (-1);
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
