fb2becb3f6065a224b470be15020ad146263eabd
calculator001 <calvinokumu05@gmail.com>
2023-04-05 12:33:57 -0700

    malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,123 +1,96 @@
-#include <stdlib.h>
 #include "main.h"
-		
-		
-/**		
- *  count_word - helper function to count the number of words in a string		
- * @s: string to evaluate		
- *	
- * Return: number of words		
- */
-		
-int count_word(char *s)
-		
-{
-		
-	int flag, c, w;	
+#include <stdlib.h>
 
-		
-	flag = 0;
+void util(char **, char *);
+void create_word(char **, char *, int, int, int);
 
-	w = 0;
-	
-		
-        	for (c = 0; s[c] != '\0'; c++)
-	
-				{
-					if (s[c] == ' ')
-						flag = 0;
-					else if (flag == 0)
-						flag = 1;	
-						w++;	
-				}
-		return (w);
-}
-		
-/**		
- * strtow - splits a string into words
- * @str: string to split		
+/**
+ *strtow - splits a string into words.
+ *@str: the string
  *
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)
+ *Return: returns a pointer to an array of strings (words)
  */
-		
 char **strtow(char *str)
-		
 {
-		
-	char **matrix, *tmp;
-		
-	int i, k = 0, len = 0, words, c = 0, start, end;
-		
+	int i, flag, len;
+	char **words;
 
-		
-	while (*(str + len))
-		
-		len++;
-		
-	words = count_word(str);
-		
-	if (words == 0)
-		
+	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
 		return (NULL);
-		
 
-		
-	matrix = (char **) malloc(sizeof(char *) * (words + 1));
-		
-	if (matrix == NULL)
-		
-		return (NULL);
-		
-
-		
-	for (i = 0; i <= len; i++)
-		
-	{
-		
-		if (str[i] == ' ' || str[i] == '\0')
-		
+	        i = flag = len = 0;
+		while (str[i])
 		{
-		
-			if (c)
-		
+			if (flag == 0 && str[i] != ' ')
+				flag = 1;
+			if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
 			{
-		
-				end = i;
-		
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
-		
-				if (tmp == NULL)
-		
-					return (NULL);
-		
-				while (start < end)
-		
-					*tmp++ = str[start++];
-		
-				*tmp = '\0';
-		
-				matrix[k] = tmp - c;
-		
-				k++;
-		
-				c = 0;
-		
+				flag = 0;
+				len++;
 			}
-		
+			i++;
 		}
-		
-		else if (c++ == 0)
-		
-			start = i;
-		
-	}
-		
 
-		
-	matrix[k] = NULL;
-		
-		
-	return (matrix);
-		
+		len += flag == 1 ? 1 : 0;
+		if (len == 0)
+			return (NULL);
+
+		        words = (char **)malloc(sizeof(char *) * (len + 1));
+			if (words == NULL)
+				  return (NULL);
+
+			util(words, str);
+			words[len] = NULL;
+			return (words);
+}
+
+/**
+ *util - a util function for fetching words into an array
+ *@words: the strings array
+ *@str: the string
+ */
+void util(char **words, char *str)
+{
+	int i, j, start, flag;
+
+	i = j = flag = 0;
+	while (str[i])
+	{
+		if (flag == 0 && str[i] != ' ')
+		{
+			start = i;
+			flag = 1;
+		}
+
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		{
+			create_word(words, str, start, i, j);
+			jflag = 0;
+										            }
+
+					       i++;
+					         }
+
+	        if (flag == 1)
+			     create_word(words, str, start, i, j);
+}
+
+/**
+ * * create_word - creates a word and insert it into the array
+ * * @words: the array of strings
+ * * @str: the string
+ * * @start: the starting index of the word
+ * * @end: the stopping index of the word
+ * * @index: the index of the array to insert the word
+ * */
+void create_word(char **words, char *str, int start, int end, int index)
+{
+	  int i, j;
+
+	    i = end - start;
+	      words[index] = (char *)malloc(sizeof(char) * (i + 1));
+
+	        for (j = 0; start < end; start++, j++)
+			     words[index][j] = str[start];
+		  words[index][j] = '\0';
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,32 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>		
 #include "main.h"
-		
-/**
- * free_grid - frees 2d array
- * @grid: 2d grid
- * @height: height dimension of grid
- * Description: frees memory of grid
- * Return: nothing		
- *		
- */
-		
-void free_grid(int **grid, int height)
-		
-{
-		
-	int i;
-		
-	
-	for (i = 0; i < height; i++)
-		
-	{
-		
-		free(grid[i]);
-		
-	}
-		
-	free(grid);
+#include <stdlib.h>
 
-       return (0);		
+/**
+ *free_grid - frees a 2 dimensional grid
+ *@grid: the grid
+ *@height: height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
 }
